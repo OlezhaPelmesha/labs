1db58354a5dfeb82f56f16ae88ba246649753324
OlezhaPelmesha <pro100oleg1200@ukr.net>
2021-10-31 20:37:53 +0200

    Commented first part of code and uncommented second part

--- 3.cpp
+++ 3.cpp
@@ -4,7 +4,7 @@
 #define USE_MATH_DEFINES
 #include <cmath>
 using namespace std;
-
+/*
 int main()
 {
 	double PI = 3.14159265358979323846;
@@ -18,10 +18,10 @@ int main()
 	b = ((10 * x + cos(135)) / pow(x, 4) / 2 + ((sin(pow(Z, 3))) * (sin(pow(Z, 3)))))-tan(3 * PI / 2);
 	printf("Result: %i", b);
 }
+*/
 
 
-
-/*int main()
+int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
@@ -33,7 +33,6 @@ int main()
 	a = (exp(3) + pow(x, 2) + 4 / y) / (cbrt(x)+1/(pow(x,2)+4));
 	printf("Result: %i", a);
 }
-*/
 /*27. Для пошиття одного костюма потрібно  s м  тканини.
 Скільки метрів тканини було в рулоні, якщо пошили  t  костюмів і залишилось  g м  тканини ? 
 int main()
